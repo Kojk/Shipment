f93f92305b2742dd1984da886adcf938aa36bec6
CSharpFri <CSharpFri@CLASS-2-08>
2018-01-26 19:12:29 +0100

    Add project files.

---
+++ Shipment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shipment s = new Shipment();
+            Item item = new Item();
+            item.Nazov = "polozka1";
+            item.PocetKusov = 3;
+            item.CenaZaKus = 5.6;
+            s.AddItem(item);
+
+            item = new Item();
+            item.Nazov = "polozka2";
+            item.PocetKusov = 4;
+            item.CenaZaKus = 1.6;
+            s.AddItem(item);
+
+            item = new Item("polozka3", 4, 2.3);    // pouzity konstructor (struct)
+            s.AddItem(item);
+
+            //priradenie notifikacni metody udalosti(eventu)
+            Notifikace n = new Notifikace();
+            s.TrackingNumbrePridane += n.Notify;
+
+            s.TrackingNumber = "122156hgj";
+
+
+            Console.ReadLine();
+        }
+       
+    }
+}
---
+++ Shipment/Shipment.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipment
+{
+    class Shipment
+    {
+        ArrayList DbPolozky = new ArrayList();
+        private string trackingNumber;
+        public delegate void OnShipmentMade(string message);
+        public event OnShipmentMade TrackingNumbrePridane;
+        public string TrackingNumber
+        {
+            get
+            {
+                return trackingNumber;
+            }
+            set
+            {
+                trackingNumber = value;
+                TrackingNumbrePridane?.Invoke(GetInfo());
+
+            }
+
+        }
+        public string GetInfo()
+        {
+            string message = "";
+            foreach (Item balik in DbPolozky)
+            {
+                message += balik.Nazov + Environment.NewLine;
+                message += balik.PocetKusov + Environment.NewLine;
+                message += balik.CenaZaKus + Environment.NewLine;  
+
+
+            }
+            return message;
+        }
+
+        public void AddItem(Item item)
+        {
+            DbPolozky.Add(item); 
+            
+        }
+
+
+    }
+}
